375596a8de65f2b1a0816f525ac009889a062af9
sanyam.harsha <sanyam.harsha@CSELAB02-01.bennett.edu.in>
2017-08-22 11:44:08 +0530

    hi this is the first trial of github

--- sample/src/sample/bubblesort.java
+++ sample/src/sample/bubblesort.java
@@ -3,6 +3,7 @@ package sample;
 public class bubblesort {
 
 	public static void main(String[] args) {
+		System.out.print("this is the first github trial");
 		// TODO Auto-generated method stub
 
 	}
